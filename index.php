0179df200816445764133a1e4a90e5ad31e0d3e6
orphevs <ticlem27270@gmail.com>
2018-04-09 17:19:16 +0200

    Init project
    Init Project + SCSS + Bootstrap

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SI-3D</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/styles/styles.css">
+</head>
+<body>
+    test si-3d
+</body>
+<!-- JS -->
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/main.js"></script>
+</html>
