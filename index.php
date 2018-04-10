b2762943cbd7be29b3743851ed4401c0abb1fe1c
orphevs <ticlem27270@gmail.com>
2018-04-10 13:18:18 +0200

    Add fonts + Start Header

--- index.php
+++ index.php
@@ -7,7 +7,58 @@
     <link rel="stylesheet" href="assets/styles/styles.css">
 </head>
 <body>
-    test si-3d
+    <section class="grid">
+        <div class="container">
+            <div class="colonne"></div>
+            <div class="colonne"></div>
+            <div class="colonne"></div>
+            <div class="colonne"></div>
+        </div>
+    </section>
+    <section class="header">
+        <div class="container menubar">
+            <img src="assets/img/logo.png" alt="" class="logo">
+            <ul class="menu">
+                <a href="#">
+                    <li>Notre vision</li>
+                </a>
+                <a href="#">
+                    <li>Notre technologie</li>
+                </a>
+                <a href="#">
+                    <li>Nous rencontrer</li>
+                </a>
+            </ul>
+        </div>
+        <video src="assets/videos/header.mp4" class="video" muted autoplay loop>
+        </video>
+        <div class="container">
+            <div class="logo-svg">
+                <svg width="700px" height="300px" viewBox="0 0 700 300" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                    <path d="M0.943767256,121.831172 C37.9744576,86.9166598 82.957386,63.5303869 135.892552,51.6723535 C188.827719,39.8143201 243.013354,42.4195308 298.449458,59.4879858 C285.200555,153.24213 237.31738,204.667357 154.799932,213.763668 C72.2824842,222.859979 20.997096,192.215813 0.943767256,121.831172 Z" id="Path" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M346.623375,237.705274 L353.168059,184.731089 C371.10648,171.284449 382.539003,162.904284 387.465629,159.590594 C394.855567,154.620059 411.541109,107.386804 462.063172,113.652664 C487.906589,116.857823 519.643658,143.401228 536.66291,168.706169 C548.009079,185.576129 558.122127,196.098266 567.002055,200.272579 L567.002055,206.940209 C538.627853,234.515262 501.05172,250.386285 454.273659,254.55328 C407.495597,258.720274 371.612169,253.104272 346.623375,237.705274 Z" id="Path-4" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <polyline id="Path-5" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round" points="472 96 550.763428 96 550.763428 176.174961 544.13313 176.174961"></polyline>
+                    <polyline id="Path-6" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round" points="470.628588 110.893229 470.628588 68.7543667 437.153569 68.7543667 412.353949 83.7486459 412.353949 125.369556"></polyline>
+                    <path d="M287.874373,212.99453 L346.486468,212.99453" id="Path-7" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M291.556973,95.8947712 L409.919906,95.8947712" id="Path-8" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M287.338207,138.524607 L401.062913,138.524607" id="Path-9" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <polyline id="Path-15" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round" points="451.139561 68.6236077 515.200054 0.349292178 641.034091 34.4864499 653.867732 59.1363219"></polyline>
+                    <polyline id="Path-15" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round" transform="translate(552.364085, 232.137158) scale(1, -1) translate(-552.364085, -232.137158) " points="451 266.274316 515.060492 198 640.894529 232.137158 653.728171 256.78703"></polyline>
+                    <path d="" id="Path-16" stroke="#6D6D6D"></path>
+                    <path d="M568.674174,178.000593 L568.674174,93.9993681" id="Path-17" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <polyline id="Path-2" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round" points="30.8818973 178.464578 30.8818973 199.98256 41.5028998 210.603563 75.471009 210.603563"></polyline>
+                    <path d="" id="Path-3" stroke="#6D6D6D"></path>
+                    <path d="" id="Path-18" stroke="#979797"></path>
+                    <polyline id="Path-19" stroke="#6D6D6D" stroke-linecap="round" stroke-linejoin="round" points="75.9494339 210.791685 75.9494339 219.813988 88.5552596 232.419814 274.045146 232.419814 286.525757 219.939203 286.525757 110.460947"></polyline>
+                </svg>
+            </div>
+            <h1>
+                Faites le <b>grand saut</b><br>
+                vers le <b>futur</b>
+            </h1>
+        </div>
+    </section>
 </body>
 <!-- JS -->
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
